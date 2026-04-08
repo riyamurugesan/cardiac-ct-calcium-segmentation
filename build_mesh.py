ccc10d4fad9681cb4dbb72e4e4278fba7c8ff0a5
Riya Murugesan <riyamuru@ad.unc.edu>
2026-04-08 17:01:41 -0400

    cleaned up build_mesh code

--- build_mesh.py
+++ build_mesh.py
@@ -2,11 +2,12 @@ import numpy as np
 from skimage import measure
 import trimesh
 
-def export_mask_as_obj(npy_path, spacing_path, output_path):
+def build_mesh(npy_path, spacing_path):
+    """Utilizes Marching Squares algorithm to create mesh from .npy array."""
     mask = np.load(npy_path)
     spacing = np.load(spacing_path)
-    #perform marching cubes algorithm - level = 0.5 so that the triangle in marching cubes is exactly halfway before 0 and 1 (calcium)
-    verts, faces, normals = measure.marching_cubes(mask, level=0.5, spacing=spacing)
+    #perform marching cubes algorithm - level = 0.5 so the triangle in marching cubes is exactly halfway before 0 and 1 (calcium)
+    verts, faces, normals, values = measure.marching_cubes(mask, level=0.5, spacing=spacing)
     #create a mesh object
     mesh = trimesh.Trimesh(vertices=verts, faces=faces, vertex_normals=normals)
-    mesh.export(output_path)
+    return mesh
